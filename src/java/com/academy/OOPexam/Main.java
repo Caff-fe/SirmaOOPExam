4ae8ea8a2a6d61f74a75ae215dfad782003505a2
Dimo Yordanov <dimoy73@gmail.com>
2024-07-21 17:01:29 +0300

    Added sample ui structe as comment

--- src/java/com/academy/OOPexam/Main.java
+++ src/java/com/academy/OOPexam/Main.java
@@ -2,6 +2,13 @@ package com.academy.OOPexam;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        /*Template for the program's end User-interface
+        Welcome to application
+        Select an action
+        1) Add user
+        2) Edit user
+        3) Fire employee
+        4) List all Employees
+        */
     }
 }

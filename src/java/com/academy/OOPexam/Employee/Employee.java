aee56d53829b476d623760e6584cb267c8ab34da
Dimo Yordanov <dimoy73@gmail.com>
2024-07-21 22:07:54 +0300

    Fixed Employee class structure

--- src/java/com/academy/OOPexam/Employee/Employee.java
+++ src/java/com/academy/OOPexam/Employee/Employee.java
@@ -7,5 +7,62 @@ public class Employee {
     private String role;
     private double salary;
 
+    public Employee(int id, String name, String department, String role, double salary) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.role = role;
+        this.salary = salary;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", department='" + department + '\'' +
+                ", role='" + role + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }

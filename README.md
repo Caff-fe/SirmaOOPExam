2a19f022f479980741d6742c85b570555d05e8f6
Dimo Yordanov <dimoy73@gmail.com>
2024-07-21 22:11:21 +0300

    Edit to ReadMe and Main files for current project progress

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # SirmaOOPExam
 My solution to the Sirma Academy OOP exam
+
+Currently the program works with hardcoded instructions,
+I need to make a user UI for ease of use
+
+This is the progress made until a little after the deadline
+I will try to finish the task
--- src/java/com/academy/OOPexam/Main.java
+++ src/java/com/academy/OOPexam/Main.java
@@ -1,8 +1,10 @@
 package com.academy.OOPexam;
 
+import com.academy.OOPexam.Employee.EmployeeService;
 import com.academy.OOPexam.resources.CsvReader;
 import com.academy.OOPexam.resources.CsvWriter;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -14,16 +16,21 @@ public class Main {
         2) Edit user
         3) Fire employee
         4) List all Employees
+        Could not finish in time for the deadline,
+        will try to at least finish what I can for tonight
         */
-
-        String path = "src/java/com/academy/OOPexam/resources/MockDatabase.csv";
 
         String[] data = {"22", "test", "temp", "wtf", "124.00"};
 
-        CsvReader reader = new CsvReader(path);
-        List<String> employees = reader.readLines();
+        CsvReader reader = new CsvReader();
+        List<String[]> employees = reader.readLines();
 
-        System.out.println(employees);
+        for (String[] employee : employees) {
+            System.out.println(Arrays.toString(employee));
+        }
+
+        EmployeeService service = new EmployeeService();
+        service.findEmployeebyId(2);
 
 //        CsvWriter writer = new CsvWriter(path);
 //        writer.writeToCsv(data, true);

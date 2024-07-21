252e49a666f24e21cefccada70af160572d208dd
Dimo Yordanov <dimoy73@gmail.com>
2024-07-21 17:02:05 +0300

    Added employee class and db file

---
+++ src/java/com/academy/OOPexam/Employee/Employee.java
@@ -0,0 +1,11 @@
+package com.academy.OOPexam.Employee;
+
+public class Employee {
+    private int id;
+    private String name;
+    private String department;
+    private String role;
+    private double salary;
+
+
+}

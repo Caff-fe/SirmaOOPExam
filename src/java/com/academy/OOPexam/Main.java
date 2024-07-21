2ce5df617d18d3ab64ec5dab673ff810039ddf2f
Dimo Yordanov <dimoy73@gmail.com>
2024-07-21 20:36:42 +0300

    Main file used for reader/writer testing

--- src/java/com/academy/OOPexam/Main.java
+++ src/java/com/academy/OOPexam/Main.java
@@ -1,5 +1,10 @@
 package com.academy.OOPexam;
 
+import com.academy.OOPexam.resources.CsvReader;
+import com.academy.OOPexam.resources.CsvWriter;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         /*Template for the program's end User-interface
@@ -10,5 +15,19 @@ public class Main {
         3) Fire employee
         4) List all Employees
         */
+
+        String path = "src/java/com/academy/OOPexam/resources/MockDatabase.csv";
+
+        String[] data = {"22", "test", "temp", "wtf", "124.00"};
+
+        CsvReader reader = new CsvReader(path);
+        List<String> employees = reader.readLines();
+
+        System.out.println(employees);
+
+//        CsvWriter writer = new CsvWriter(path);
+//        writer.writeToCsv(data, true);
+
+//        System.out.println(employees);
     }
 }
